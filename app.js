240f036662cc654c47d4dc7924549be493394d2d
吃冰淇淋感冒了 <fairy_vip@2925.com>
2022-07-12 19:34:10 +0800

    fix: 优化依赖，新增0.0.3版本

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 'use strict';
 const { creatGRPClientInstance } = require('./libs');
-module.exports = async app => {
+const assert = require('assert');
+module.exports = app => {
   const now = new Date().getTime();
   const config = app.config.grpcClient;
-  if (config == null) {
-    app.logger.error(`[egg-grpc-client] no grpcClient options in config file! (${new Date().getTime() - now}ms)`);
-    return;
-  }
-  // todo 考虑挂载到ctx上
-  // const client = await creatGRPClientInstance(config, app);
-  // app.addSingleton('grpcClient', await creatGRPClientInstance);
-  app.grpcClient = await creatGRPClientInstance(config, app);
+  assert(typeof config.app === 'boolean', `[egg-grpc-client] app just is true or false , but now agent is ${config.agent}`);
+  assert(!(config.client == null && config.clients == null) || (config.client != null && config.clients != null), '[egg-grpc-client] only one can exist between client and clients in config.js file');
+  // 启动之前
+  app.beforeStart(async () => {
+    if (config.app) {
+      // 加载到agent对象上，创建一个实例
+      app.addSingleton('grpcClient', await creatGRPClientInstance);
+      app.logger.info(`[egg-grpc-client] load grpc client to app.js (${new Date().getTime() - now}ms)`);
+    }
+  });
 };
 
