4734cd537410f0f7d852ca8befd5dbb00e2b84fc
吃冰淇淋感冒了 <fairy_vip@2925.com>
2022-07-16 17:29:38 +0800

    feat: 使用demo初始化，新增配置

---
+++ example/config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = appInfo => {
+
+  const config = exports = {};
+  config.keys = appInfo.name + '_142vip';
+  config.grpcClient={
+
+  }
+  return config
+};
---
+++ example/config/plugin.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    grpcClient:{
+        enable: true,
+        package: 'egg-grpc-client',
+    }
+};
